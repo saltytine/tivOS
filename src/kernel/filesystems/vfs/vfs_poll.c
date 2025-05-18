0ebd9d718d9986d9cab7923ce891fe30885540ee
Saltine <156854448+saltytine@users.noreply.github.com>
2025-05-18 00:29:33 -0400

    some stub stuff for polling (later)

---
+++ src/kernel/filesystems/vfs/vfs_poll.c
@@ -0,0 +1,22 @@
+#include <disk.h>
+#include <ext2.h>
+#include <fat32.h>
+#include <malloc.h>
+#include <poll.h>
+#include <string.h>
+#include <syscalls.h>
+#include <system.h>
+#include <task.h>
+#include <unixSocket.h>
+#include <util.h>
+#include <vfs.h>
+
+// VFS-sided implementation of generic *poll() functions
+
+// todo: think this through as pairs span across more than one fd (think of
+// pipes with read/write ends, etc)
+void fsInformReady(OpenFile *fd, int epollEvents) {
+  spinlockAcquire(&fd->LOCK_POLL);
+  // do nothing
+  spinlockRelease(&fd->LOCK_POLL);
+}
